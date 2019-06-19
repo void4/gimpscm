b2702e73b7399cdc95a268b4e74aa0ad25b58ff5
Noreply <noreply@github.com>
2019-06-19 17:12:28 +0200

    v0.9

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'gimpscm',         # How you named your package folder (MyLib)
   packages = ['gimpscm'],   # Chose the same as "name"
-  version = '0.8',      # Start with a small number and increase it with every change you make
+  version = '0.9',      # Start with a small number and increase it with every change you make
   license='CC BY-NC 3.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Automatically generate and execute gimp script files',   # Give a short description about your library
   author = 'void4',                   # Type in your name
   author_email = 'void4.noreply@github.io',      # Type in your E-Mail
   url = 'https://github.com/void4/gimpscm',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/void4/gimpscm/archive/0.8.tar.gz',
+  download_url = 'https://github.com/void4/gimpscm/archive/0.9.tar.gz',
   keywords = ['gimp', 'image', 'editing'],   # Keywords that define your package best
   install_requires=[],
   classifiers=[
@@ -24,4 +24,5 @@ setup(
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
   ],
+  package_data = {"gimpscm": ["gimpscm/procdump.txt"]},
 )
