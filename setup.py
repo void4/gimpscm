9c8f2a0770c195979f350dee3d87a44818670a01
Noreply <noreply@github.com>
2019-06-19 14:54:20 +0200

    Prepare pip distribution

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'gimpscm',         # How you named your package folder (MyLib)
+  packages = ['gimpscm'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='CC BY-NC 3.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Automatically generate and execute gimp script files',   # Give a short description about your library
+  author = 'void4',                   # Type in your name
+  author_email = 'void4.noreply@github.io',      # Type in your E-Mail
+  url = 'https://github.com/void4/gimpscm',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/void4/gimpscm/archive/0.1.tar.gz',    # I explain this later on
+  keywords = ['gimp', 'image', 'editing'],   # Keywords that define your package best
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+
+    'License :: OSI Approved :: CC BY-NC 3.0 License',   # Again, pick a license
+
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
